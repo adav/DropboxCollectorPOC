414ef63a22afe31e240d5da1aabcf62dd8c76a00
Andrew Davidson <andrew.davidson@elsevier.com>
2016-05-23 13:18:15 +0100

    list files

--- app/controllers/DropboxController.scala
+++ app/controllers/DropboxController.scala
@@ -2,9 +2,12 @@ package controllers
 
 import javax.inject._
 
+import play.api.libs.json.JsLookupResult
 import play.api.mvc._
 import play.twirl.api.Html
-import services.DropboxService
+import services.{CurrentDropboxUser, DropboxService}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class DropboxController @Inject()(dropbox: DropboxService) extends Controller {
@@ -17,10 +20,21 @@ class DropboxController @Inject()(dropbox: DropboxService) extends Controller {
     Ok("Authorised" + response.queryString)
   }
 
-  def listFiles = Action {
-    val html = Html("hi")
+  def listFiles = Action.async {
+    dropbox.getFilesInFolder(CurrentDropboxUser.currentUser.token, "/").map{ json =>
+      val files = json \\ "path"
 
-    Ok(views.html.Application.main("Your data files")(html))
+      val listOfFiles = files
+        .map(_.as[String])
+        .filter(_.contains("."))
+        .map("<li>" + _ + "</li>")
+
+      val html = Html(json.toString() + listOfFiles.mkString("<ul>", "\n", "</ul>"))
+
+      Ok(views.html.Application.main("Your data files")(html))
+
+    }
+
   }
 
 }
--- app/services/DropboxService.scala
+++ app/services/DropboxService.scala
@@ -5,6 +5,7 @@ import javax.inject._
 
 import akka.actor.ActorRef
 import play.api.Logger
+import play.api.libs.json.JsValue
 import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent._
@@ -56,26 +57,13 @@ class DropboxService @Inject()(ws: WSClient, @Named("new-user-actor") userActor:
 
   }
 
-  def getFilesInFolder = {
-    ws.url("https://api.dropboxapi.com/1/oauth2/token")
-      .post(Map(
-        "code" -> Seq(code),
-        "grant_type" -> Seq("authorization_code"),
-        "client_id" -> Seq(DropboxService.appKey),
-        "client_secret" -> Seq(DropboxService.appSecret),
-        "redirect_uri" -> Seq("http://localhost:9000/dropbox_redirect")
-      )) onComplete {
-      _ map { response =>
-
-        val json = response.json
-        val user = DropboxUser(
-          (json \ "uid").as[String],
-          (json \ "access_token").as[String]
-        )
-
-        userActor ! user
-
-      }
-    }
+  def getFilesInFolder(token: String, path: String = "/"): Future[JsValue] = {
+    ws.url("https://api.dropboxapi.com/1/metadata/auto" + path)
+      .withQueryString(
+        "list" -> "true",
+        "include_deleted" -> "false",
+        "include_media_info" -> "true"
+      ).withHeaders("Authorization" -> ("Bearer " + token))
+      .get map(_.json)
   }
 }
